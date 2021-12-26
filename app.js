75971f5e079756a7a272514b6c5678bcdd94474b
Dilshod Musakhanov <musakhanov@yahoo.com>
2021-12-26 06:17:20 +0400

    update slider range as per bg color

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ function randomColors() {
 
         colorizeSliders(color,hue,brightness,saturation);
     });
+
+    //reset inputs
+    resetInputs();
 }
 
 function checkTextContrast(color,text){
@@ -113,6 +116,30 @@ function updateTextUI(index) {
     for(icon of icons) {
         checkTextContrast(color, icon);
     }
+}
+
+function resetInputs(){
+    const sliders = document.querySelectorAll(".sliders input");
+    sliders.forEach(slider =>{
+        if(slider.name === 'hue') {
+            const hueColor = initialColors[slider.getAttribute("data-hue")];
+            const hueValue = chroma(hueColor).hsl()[0];
+            slider.value = Math.floor(hueValue);
+        }
+        if(slider.name === 'saturation') {
+            const satColor = initialColors[slider.getAttribute("data-sat")];
+            const satValue = chroma(satColor).hsl()[1];
+            slider.value = Math.floor(satValue *100) /100;
+        }
+        if(slider.name === 'brightness') {
+            const brightColor = initialColors[slider.getAttribute("data-bright")];
+            const brightValue = chroma(brightColor).hsl()[2];
+            slider.value = Math.floor(brightValue *100) /100;
+        }
+
+    })
+
+
 }
 
 randomColors();
